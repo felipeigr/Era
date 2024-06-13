a13de386839c563470679ca794ff5cff18a99201
felipe <149977529+felipeigr@users.noreply.github.com>
2024-06-12 21:28:21 -0400

    Proyecto Django 1.1
    todas las vistas listas

--- Proyecto/productos/urls.py
+++ Proyecto/productos/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('valo/', views.valo, name='valo'),
     path('lol/', views.lol, name='lol'),
+    path('login/', views.login, name='login'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('rp/', views.rp, name='rp'),
+    path('carrito/', views.carrito, name='carrito'),
 ]
 
--- Proyecto/productos/views.py
+++ Proyecto/productos/views.py
@@ -11,7 +11,32 @@ def valo(request):
     context = {'productos': productos}
     return render(request, 'productos/valo.html', context)
 
+
 def lol(request):
     productos = Producto.objects.all()  # Reutilizamos la lógica para obtener los productos
     context = {'productos': productos}
     return render(request, 'productos/lol.html', context)
+
+
+def login(request):
+    productos = Producto.objects.all()  # Reutilizamos la lógica para obtener los productos
+    context = {'productos': productos}
+    return render(request, 'productos/login.html', context)
+
+
+def nosotros(request):
+    productos = Producto.objects.all()  # Reutilizamos la lógica para obtener los productos
+    context = {'productos': productos}
+    return render(request, 'productos/nosotros.html', context)
+
+
+def rp(request):
+    productos = Producto.objects.all()  # Reutilizamos la lógica para obtener los productos
+    context = {'productos': productos}
+    return render(request, 'productos/rp.html', context)
+
+
+def carrito(request):
+    productos = Producto.objects.all()  # Reutilizamos la lógica para obtener los productos
+    context = {'productos': productos}
+    return render(request, 'productos/carrito.html', context)
